f6ac08487a6d90e08dd0217813f1311d450a41d0
Martin Weinzierl <152305632+execvpe@users.noreply.github.com>
2021-11-27 17:46:52 +0100

    [Style] Add comments

--- src/TSensor.cpp
+++ src/TSensor.cpp
@@ -1,10 +1,14 @@
 #include "TSensor.hpp"
 
+// public constructors
+
 TSensor::TSensor() {
 	oneWire = OneWire(SENSOR_BUS_PIN);
 	sensors = DallasTemperature(&oneWire);
 	sensors.begin();
 }
+
+// public functions
 
 float TSensor::getCelsius(uint8_t sensorIdx) {
 	sensors.requestTemperaturesByIndex(sensorIdx);
